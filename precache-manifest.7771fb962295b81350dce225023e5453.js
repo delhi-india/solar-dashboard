e79783daf6a6feed8615b865ed5840f2978bd37a
Abhishek Upperwal <a.upperwal@gmail.com>
2020-01-27 14:01:51 +0530

    Updates

---
+++ precache-manifest.7771fb962295b81350dce225023e5453.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db777d1e391d11c75bfaa91ba7bfc181",
+    "url": "/solar-dashboard/index.html"
+  },
+  {
+    "revision": "1b9aa48c37f5aeaae886",
+    "url": "/solar-dashboard/static/css/2.c7d0a5a8.chunk.css"
+  },
+  {
+    "revision": "c76b7833c8704809ea3b",
+    "url": "/solar-dashboard/static/css/main.54da51f7.chunk.css"
+  },
+  {
+    "revision": "1b9aa48c37f5aeaae886",
+    "url": "/solar-dashboard/static/js/2.0893965f.chunk.js"
+  },
+  {
+    "revision": "eb85a117688019f64d7252bfa6339878",
+    "url": "/solar-dashboard/static/js/2.0893965f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c76b7833c8704809ea3b",
+    "url": "/solar-dashboard/static/js/main.13bee995.chunk.js"
+  },
+  {
+    "revision": "150bde2445632a5ae8e3",
+    "url": "/solar-dashboard/static/js/runtime-main.95dc24f5.js"
+  },
+  {
+    "revision": "5b333429952245f10b55661fee3b39d4",
+    "url": "/solar-dashboard/static/media/cube.5b333429.svg"
+  },
+  {
+    "revision": "48606cef82e42ef01d69839ffdbfbf02",
+    "url": "/solar-dashboard/static/media/github.48606cef.svg"
+  },
+  {
+    "revision": "70283587554291a6e873a36d3443017b",
+    "url": "/solar-dashboard/static/media/twitter.70283587.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/solar-dashboard/precache-manifest.7771fb962295b81350dce225023e5453.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solar-dashboard/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
